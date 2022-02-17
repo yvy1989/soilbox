a1fa7d89006061fe8404bd5cd7d28603ff9cc7b0
andersonyvy <32403616+andersonyvy@users.noreply.github.com>
2022-02-17 19:15:43 -0400

    blocagem
    Movimento de camera e cursor ok

---
+++ Assets/Scripts/CameraControll.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// creditos https://satellasoft.com/artigo/unity-3d/camera-rts-com-a-unity-3d
+/// newking777 
+/// Responsavel por movimentar a camera e aplicar o zoom
+/// </summary>
+
+public class CameraControll : MonoBehaviour
+{
+    public float tamanhoBorda = 25.0f;
+    public float velocidade = 10.0f;
+
+    private float screenX;
+    private float screenY;
+
+    public float limiteHorizontalEsquerdo;
+    public float limiteHorizontalDireito;
+
+    public float limiteVerticalSuperior;
+    public float limiteVerticalInferior;
+
+
+
+    void Start()
+    {
+        screenX = Screen.width;
+        screenY = Screen.height;
+    }
+
+    void Update()
+    {
+
+        MoveCam();
+        zoomScroll();
+
+    }
+
+    private static void zoomScroll()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if (Camera.main.fieldOfView > 1)
+            {
+                Camera.main.fieldOfView--;
+            }
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if (Camera.main.fieldOfView < 100)
+            {
+                Camera.main.fieldOfView++;
+            }
+        }
+    }
+
+    private void MoveCam()
+    {
+        Vector3 posicao = Vector3.zero;
+
+        if (Input.mousePosition.x < tamanhoBorda && transform.position.z >= limiteHorizontalEsquerdo)
+        {
+            posicao.z = -1;
+        }
+        if (Input.mousePosition.x > (screenX - tamanhoBorda) && transform.position.z <= limiteHorizontalDireito)
+        {
+            posicao.z = 1;
+        }
+
+        if (Input.mousePosition.y < 25 && transform.position.x <= limiteVerticalInferior)
+            posicao.x = 1;
+
+        if (Input.mousePosition.y > (screenY - tamanhoBorda) && transform.position.x >= limiteVerticalSuperior)
+            posicao.x = -1;
+
+        if (posicao == Vector3.zero)
+            posicao = GetKeyPosition();
+
+        transform.Translate(posicao * velocidade * Time.deltaTime);
+
+
+    }
+
+    private Vector3 GetKeyPosition()
+    {
+        return new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    }
+}
---
+++ Assets/Scripts/SoilBehavior.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoilBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseEnter()
+    {
+        GetComponent<MeshRenderer>().enabled = true;
+        //Debug.Log("passou");
+    }
+    private void OnMouseExit()
+    {
+        GetComponent<MeshRenderer>().enabled = false;
+        //Debug.Log("saiu");
+    }
+}
