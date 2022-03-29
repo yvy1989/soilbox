e1284e9d36bdc2192553088c22767699944d9086
yvy1989 <andersonyvy1@gmail.com>
2022-03-29 19:42:22 -0400

    cena salva

--- Assets/Scripts/UiController.cs
+++ Assets/Scripts/UiController.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UiController : MonoBehaviour
 {
+    public Image CreditAmount;
+    public Image CarbonAmount;
+
     // Start is called before the first frame update
     void Start()
     {
