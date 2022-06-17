2fa142717cdf29dbeb1a44adc1e170bfc2c69104
yvy1989 <99916213+yvy1989@users.noreply.github.com>
2022-06-16 21:30:34 -0400

    particula moeda vaca

--- Assets/Scripts/UnitValueOverTime.cs
+++ Assets/Scripts/UnitValueOverTime.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class UnitValueOverTime : MonoBehaviour
 {
+
     /// <summary>
     /// /Adiciona ou remove valores de acordo com o tempo ex: vacas vao adicionando dinheiro e removendo carbono com o tempo
     /// </summary>
     // Start is called before the first frame update
 
     public float timeMoney, timeMaxCarbon, CarbonAmount, MoneyAmount;
+
+    public GameObject unit;
+    public GameObject ParticleCoin;
+
+    
     void Start()
     {
+        unit = transform.GetChild(0).gameObject;
+
         StartCoroutine(AddAndRemoveValues(timeMoney, timeMaxCarbon, CarbonAmount, MoneyAmount));
     }
 
@@ -23,12 +31,17 @@ public class UnitValueOverTime : MonoBehaviour
 
     IEnumerator AddAndRemoveValues(float _timeMoney,float _timeMaxCarbon,float _CarbonAmount,float _MoneyAmount)
     {
+        
         float tempCarbon = 0;
         while (true)
         {
+            GameObject coin;
             yield return new WaitForSeconds(_timeMoney);
 
             GameManager.Instance.addMoney(_MoneyAmount);
+
+
+            coin = Instantiate(ParticleCoin, new Vector3(unit.transform.position.x, unit.transform.position.y+1, unit.transform.position.z),Quaternion.Euler(-90, -90, 0));
 
             if (tempCarbon > _timeMaxCarbon)
             {
@@ -36,6 +49,8 @@ public class UnitValueOverTime : MonoBehaviour
                 tempCarbon = 0;
             }
 
+            yield return new WaitForSeconds(1f);
+            Destroy(coin);
             tempCarbon++;
         }
 
